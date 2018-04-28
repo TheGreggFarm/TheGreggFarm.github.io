66c450475f280f4d85c0e8fd43a36e58b5923fa0
TheGreggFarm <36521541+TheGreggFarm@users.noreply.github.com>
2018-04-27 21:06:24 -0400

    test

--- scripts/farm.js
+++ scripts/farm.js
@@ -38,9 +38,7 @@ var html= ""
 html += "<div id=\"carousel\" class=\"carousel slide\" data-ride=\"carousel\">"
 html += "    <div class=\"carousel-inner\">"
 html += "    <div class=\"carousel-item active\">"
-    var str1= "";
-    str1 = carArray[0].text;
-    alert(str1);
+   
 html += "       <img class=\"d-block w-100\"  src=\"\images\\"+ carArray[0] +".jpg\" alt=\"First Slide\">"
 html += "       </div>"
 for (i = 1; i< carArray.length;i++){
