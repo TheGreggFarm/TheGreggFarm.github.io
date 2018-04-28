229de44d94c55e3926041455a5659079cc94061b
TheGreggFarm <36521541+TheGreggFarm@users.noreply.github.com>
2018-04-28 07:22:33 -0400

    test

--- scripts/farm.js
+++ scripts/farm.js
@@ -39,11 +39,11 @@ html += "<div id=\"carousel\" class=\"carousel slide\" data-ride=\"carousel\">"
 html += "    <div class=\"carousel-inner\">"
 html += "    <div class=\"carousel-item active\">"
    
-html += "       <img class=\"d-block w-100\"  src=\"..\images\\"+ carArray[0] +".jpg\" alt=\"First Slide\">"
+html += "       <img class=\"d-block w-100\"  src=\"images\\"+ carArray[0] +".jpg\" alt=\"First Slide\">"
 html += "       </div>"
 for (i = 1; i< carArray.length;i++){
     html += "      <div class=\"carousel-item\">"
-    html += "       <img class=\"d-block w-100\"  src=\"..\images\\"+ carArray[i] +".jpg\" alt=\"Second Slide\">"
+    html += "       <img class=\"d-block w-100\"  src=\"images\\"+ carArray[i] +".jpg\" alt=\"Second Slide\">"
     html += "       </div>"
         
        
